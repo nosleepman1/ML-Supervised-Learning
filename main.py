c2230a5dd668bef88ccb15d46597349a2bc3d6f8
nosleepman1 <abdallahdiouf.dev@gmail.com>
2026-06-17 22:37:36 +0000

    add main file

---
+++ main.py
@@ -0,0 +1,13 @@
+import pandas as pd
+#import matplotlib.pyplot as plt
+
+
+data = pd.read_csv('ecommerce_recommendation_dataset.csv')
+ 
+ 
+# on datas session duration (s) also session(user connexion)
+# device and ip address
+# naviation time (check product)
+# clients ratings (from 1 to five) or %
+# every app user with his wishlist count
+# count rows 12k
